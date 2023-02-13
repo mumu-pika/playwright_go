6ba8ffb7966f795a0d467901dadc44b5a9872e72
mumu <rmli1912@foxmail.com>
2023-02-13 18:06:27 +0800

    chore(pkg): add some scripts in package.json

--- tests/counter.spec.ts
+++ tests/counter.spec.ts
@@ -1,7 +1,12 @@
-// https://playwright.dev/docs/writing-tests
+// // https://playwright.dev/docs/writing-tests
 // import { test, expect } from '@playwright/experimental-ct-vue'
 // import Counter from '@/components/Counter.vue'
-// test ('Counter render', async({mount}) => {
-//   const component = await mount(Counter)
+// import HelloWorld from '../src/components/HelloWorld.vue'
+// // test ('Counter render', async({mount}) => {
+// //   const component = await mount(Counter)
+// //   await expect(component).toContainText('Counter')
+// // })
+// test ('HelloWord render', async({mount}) => {
+//   const component = await mount(HelloWorld)
 //   await expect(component).toContainText('Counter')
 // })
